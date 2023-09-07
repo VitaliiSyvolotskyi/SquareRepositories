48d61feec2cc89069b3730f2d0028164465404c2
vitalii <syvolotskyi@zfort.com>
2023-09-07 18:41:39 +0300

    # updated design for list

--- app/src/main/java/com/andersenlab/poq/di/NetworkModule.kt
+++ app/src/main/java/com/andersenlab/poq/di/NetworkModule.kt
@@ -1,53 +1,24 @@
 package com.andersenlab.poq.di
 
-import android.content.Context
-import androidx.viewbinding.BuildConfig
 import com.andersenlab.poq.Constants
 import com.andersenlab.poq.data.api.RepositoriesApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
-    @Singleton
-    @Provides
-    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
-        val logging = HttpLoggingInterceptor()
-        logging.level =
-            if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
-            else HttpLoggingInterceptor.Level.NONE
-        return logging
-    }
-
-    @Singleton
-    @Provides
-    fun provideOkHttpClient(
-        @ApplicationContext appContext: Context,
-        loggingInterceptor: HttpLoggingInterceptor
-    ): OkHttpClient =
-        OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .build()
 
     @Singleton
     @Provides
     fun provideRetrofit(
-        okHttpClient: OkHttpClient
     ): Retrofit = Retrofit.Builder()
         .baseUrl(Constants.BASE_URL)
-        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
